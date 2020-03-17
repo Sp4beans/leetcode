fe8633c8597abd37f44aab37738bbcb37c24cb85
Sp4beans <sp4beans@gmail.com>
2020-03-17 11:28:58 +0800

    add test

---
+++ src/main/java/com/sp4beans/leetcode/Test.java
@@ -0,0 +1,11 @@
+package com.sp4beans.leetcode;
+
+/**
+ * @author wangjixin <wangjixin@kuaishou.com>
+ * Created on 2020-03-16
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
