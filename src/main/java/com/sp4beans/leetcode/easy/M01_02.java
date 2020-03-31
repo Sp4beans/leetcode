58abf80f7c01b121f5ad76f140c707564d096c93
wangjixin <wangjixin@kuaishou.com>
2020-03-31 23:34:28 +0800

    add 0102 0103

--- src/main/java/com/sp4beans/leetcode/easy/M01_02.java
+++ src/main/java/com/sp4beans/leetcode/easy/M01_02.java
@@ -15,9 +15,29 @@ package com.sp4beans.leetcode.easy;
 //        0 <= len(s1) <= 100
 //        0 <= len(s2) <= 100
 
+import java.util.Arrays;
+
 public class M01_02 {
 
-    public static void main(String[] args) {
-
+    private class Solution {
+        public boolean CheckPermutation(String s1, String s2) {
+            if (s1 == null || s2 == null) {
+                return s1 == s2;
+            }
+            if (s1.length() != s2.length()) {
+                return false;
+            }
+            char[] cs1 = s1.toCharArray();
+            char[] cs2 = s2.toCharArray();
+            Arrays.sort(cs1);
+            Arrays.sort(cs2);
+            for (int i = 0; i < cs1.length; i++) {
+                if (cs1[i] != cs2[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
+
 }
