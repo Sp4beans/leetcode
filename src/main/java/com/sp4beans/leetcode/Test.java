1ca4b58a91f5a38d44bd64fd85dc4e0ef28a63a7
Sp4beans <sp4beans@gmail.com>
2020-07-28 14:27:55 +0800

    update

--- src/main/java/com/sp4beans/leetcode/Test.java
+++ src/main/java/com/sp4beans/leetcode/Test.java
@@ -1,8 +1,11 @@
 package com.sp4beans.leetcode;
 
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.Method;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @author wangjixin <wangjixin@kuaishou.com>
@@ -10,30 +13,32 @@ import java.lang.reflect.Method;
  */
 public class Test {
 
-    public static class KV {
-        private int x;
-
-        public Type getX() {
-            return null;
-        }
-
-        public void setX(int x) {
-
-        }
-    }
-
-    public enum Type {
-        A,
-        B,
-        C;
-    }
-
     public static void main(String[] args) throws Exception {
-        PropertyDescriptor[] pds = Introspector.getBeanInfo(KV.class).getPropertyDescriptors();
-        for (PropertyDescriptor pd : pds) {
-            Method writeMethod = pd.getWriteMethod();
-            System.out.println(pd.getName() + ":" + ((writeMethod == null) ? "null" : writeMethod.getName()));
+
+        File file1 = new File("/Users/wangjixin/Desktop/monthly_detail.xlsx");
+        File file2 = new File("/Users/wangjixin/Desktop/monthly_detail_back.xlsx");
+
+        byte[] buf = new byte[1024];
+
+        File outputFile = new File("/Users/wangjixin/Desktop/test.zip");
+        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(outputFile));
+
+
+        for (File file : Arrays.asList(file1, file2)) {
+            zos.putNextEntry(new ZipEntry(file.getName()));
+            FileInputStream fis = new FileInputStream(file);
+            int len;
+            while ((len = fis.read(buf)) > 0) {
+                zos.write(buf, 0, len);
+            }
+            zos.closeEntry();
+            fis.close();
         }
+
+        zos.flush();
+        zos.close();
+
     }
+
 }
 
