ec884cb3372b9ebdd73334f2c2752fd89b77742e
Sp4beans <sp4beans@gmail.com>
2020-08-03 11:41:52 +0800

    update

--- src/main/java/com/sp4beans/leetcode/medium/P740.java
+++ src/main/java/com/sp4beans/leetcode/medium/P740.java
@@ -27,9 +27,40 @@ package com.sp4beans.leetcode.medium;
 //        每个整数nums[i]的大小都在[1, 10000]范围内。
 
 public class P740 {
-    private class Solution {
-        public int deleteAndEarn(int[] nums) {
+    private static class Solution {
+        private int len = 10001;
 
+        public int deleteAndEarn(int[] nums) {
+            int[] count = new int[len];
+            int max = 1;
+            for (int num : nums) {
+                count[num]++;
+                max = Math.max(max, num + 1);
+            }
+
+            int[] dp = new int[max];
+            for (int i = 1; i < max; i++) {
+                if (count[i] == 0) {
+                    dp[i] = dp[i - 1];
+                } else if (count[i] > 0) {
+                    if (i <= 2) {
+                        dp[i] = Math.max(dp[i - 1], i * count[i]);
+                    } else {
+                        dp[i] = Math.max(dp[i - 1], dp[i - 2] + i * count[i]);
+                    }
+                }
+            }
+            return dp[max - 1];
         }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.deleteAndEarn(
+                new int[] {12, 32, 93, 17, 100, 72, 40, 71, 37, 92, 58, 34, 29, 78, 11, 84, 77, 90, 92, 35, 12, 5, 27,
+                        92, 91, 23, 65, 91, 85, 14, 42, 28, 80, 85, 38, 71, 62, 82, 66, 3, 33, 33, 55, 60, 48, 78, 63,
+                        11, 20, 51, 78, 42, 37, 21, 100, 13, 60, 57, 91, 53, 49, 15, 45, 19, 51, 2, 96, 22, 32, 2, 46,
+                        62, 58, 11, 29, 6, 74, 38, 70, 97, 4, 22, 76, 19, 1, 90, 63, 55, 64, 44, 90, 51, 36, 16, 65, 95,
+                        64, 59, 53, 93}));
     }
 }
