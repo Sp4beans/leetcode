281d8ad068940db0ed9a5a212cbcbbd58321e25b
Sp4beans <sp4beans@gmail.com>
2020-08-09 13:36:31 +0800

    update

--- src/main/java/com/sp4beans/leetcode/Test.java
+++ src/main/java/com/sp4beans/leetcode/Test.java
@@ -1,124 +1,14 @@
 package com.sp4beans.leetcode;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-
 /**
  * @author wangjixin <wangjixin@kuaishou.com>
  * Created on 2020-05-13
  */
 public class Test {
 
-    private class InspectionArticleLogModel {
-        private long articleId;
+    public static void main(String[] args) {
 
-        public long getArticleId() {
-            return articleId;
-        }
-
-        public void setArticleId(long articleId) {
-            this.articleId = articleId;
-        }
-    }
-
-    private class ArticleContentModel {
-        private long articleId;
-
-        public long getArticleId() {
-            return articleId;
-        }
-
-        public void setArticleId(long articleId) {
-            this.articleId = articleId;
-        }
-    }
-
-    private class ArticleWarnMessageModel {
-        private long articleId;
-
-        public long getArticleId() {
-            return articleId;
-        }
-
-        public void setArticleId(long articleId) {
-            this.articleId = articleId;
-        }
-    }
-
-    private class ArticleIndicatorsModel {
-        private long articleId;
-
-        public long getArticleId() {
-            return articleId;
-        }
-
-        public void setArticleId(long articleId) {
-            this.articleId = articleId;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        // 通过查询语句把这玩意搞出来
-        List<InspectionArticleLogModel> articleLogModelList = queryLogModels();
-
-        List<Long> articleIds = articleLogModelList.stream()
-                .map(InspectionArticleLogModel::getArticleId)
-                .distinct()
-                .collect(Collectors.toList());
-
-        // 假设这俩是一对一的
-        Map<Long, ArticleContentModel> idToArticleContent = Maps.newHashMap();
-        Map<Long, ArticleWarnMessageModel> idToArticleWarnMessage = Maps.newHashMap();
-
-        // 假设这玩意是一对多的
-        Map<Long, List<ArticleIndicatorsModel>> idToArticleIndicators = Maps.newHashMap();
-
-        // 读mysql的时候即使有索引 也别一次查大量id 一次查1000条 多查几次就完事了
-        Lists.partition(articleIds, 1000).forEach(subList -> {
-            idToArticleContent.putAll(
-                    queryArticleContentsByIds(subList).stream()
-                            .collect(Collectors.toMap(ArticleContentModel::getArticleId, Function.identity(), (a, b) -> b))
-            );
-
-            idToArticleWarnMessage.putAll(
-                    queryArticleWarnMessageByIds(subList).stream()
-                            .collect(Collectors.toMap(ArticleWarnMessageModel::getArticleId, Function.identity(), (a, b) -> b))
-            );
-
-            idToArticleIndicators.putAll(
-                    queryArticleIndicatorsByIds(subList).stream()
-                            .collect(Collectors.groupingBy(ArticleIndicatorsModel::getArticleId))
-            );
-        });
-
-        // TODO 剩下的就是根据 articleLogModelList 拼结果了 不写了
-    }
-
-    private static List<InspectionArticleLogModel> queryLogModels() {
-        // TODO do sth.
-        return Lists.newArrayList();
-    }
-
-    private static List<ArticleContentModel> queryArticleContentsByIds(Collection<Long> articleIds) {
-        // TODO do sth.
-        return Lists.newArrayList();
-    }
-
-    private static List<ArticleWarnMessageModel> queryArticleWarnMessageByIds(Collection<Long> articleIds) {
-        // TODO do sth.
-        return Lists.newArrayList();
-    }
-
-    private static List<ArticleIndicatorsModel> queryArticleIndicatorsByIds(Collection<Long> articleIds) {
-        // TODO do sth.
-        return Lists.newArrayList();
+        System.exit(0);
     }
 
 }
---
+++ src/main/java/com/sp4beans/leetcode/easy/P5468.java
@@ -0,0 +1,55 @@
+package com.sp4beans.leetcode.easy;
+
+//        给你一个 严格升序排列 的正整数数组 arr 和一个整数 k 。
+//
+//        请你找到这个数组里第 k 个缺失的正整数。
+//
+//         
+//
+//        示例 1：
+//
+//        输入：arr = [2,3,4,7,11], k = 5
+//        输出：9
+//        解释：缺失的正整数包括 [1,5,6,8,9,10,12,13,...] 。第 5 个缺失的正整数为 9 。
+//        示例 2：
+//
+//        输入：arr = [1,2,3,4], k = 2
+//        输出：6
+//        解释：缺失的正整数包括 [5,6,7,...] 。第 2 个缺失的正整数为 6 。
+//         
+//
+//        提示：
+//
+//        1 <= arr.length <= 1000
+//        1 <= arr[i] <= 1000
+//        1 <= k <= 1000
+//        对于所有 1 <= i < j <= arr.length 的 i 和 j 满足 arr[i] < arr[j] 
+
+public class P5468 {
+    private static class Solution {
+        public int findKthPositive(int[] arr, int k) {
+            int base = 1;
+            int idx = 0;
+            while (k > 0 && idx < arr.length) {
+                int cur = arr[idx];
+                if (base < cur) {
+                    k--;
+                } else {
+                    idx++;
+                }
+                base++;
+            }
+            while (k-- > 0) {
+                base++;
+            }
+            return base - 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findKthPositive(new int[] {2, 3, 4, 7, 11}, 5));
+        System.out.println(solution.findKthPositive(new int[] {1, 2, 3, 4}, 2));
+        System.out.println(solution.findKthPositive(new int[] {1, 2}, 1));
+    }
+}
